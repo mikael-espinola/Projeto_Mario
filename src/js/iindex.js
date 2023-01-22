7ace94e2dbbd28e1d40bef258d73391aa4862df5
mikael-espinola <mikaelespinolaa@gmail.com>
2023-01-21 22:06:26 -0300

    Ãdicionado função ao botão do trailer

---
+++ src/js/iindex.js
@@ -0,0 +1,18 @@
+const botaoTrailer = document.querySelector('.botao-trailer');
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDovideo = video.src
+
+function alternarModal (){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDovideo);
+});
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
